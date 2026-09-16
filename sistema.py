5aa950c25709d5003cf42324635759fa677a7c56
HeitorJorge019 <heitorjorge015@gmail.com>
2026-09-16 11:13:33 -0300

    Update sistema.py

--- sistema.py
+++ sistema.py
@@ -15,3 +15,16 @@ def cadastrar():
     cadastros.append(usuario)
 
     print("\nCadastro realizado com sucesso!")
+
+
+def consultar():
+    print("\nCONSULTAR")
+
+    if len(cadastros) == 0:
+        print("Nenhum cadastro encontrado.")
+        return
+
+    for i, usuario in enumerate(cadastros, start=1):
+        print(f"\nID: {i}")
+        print(f"Nome: {usuario['nome']}")
+        print(f"E-mail: {usuario['email']}")
